2ff2965dfa2427208bd002d09b6efc28b63f4e75
Dylan Larrabee <dylan.r.larrabee@gmail.com>
2017-01-17 10:25:06 -0800

    Add local passport strategy in signup route

--- app/routes.js
+++ app/routes.js
@@ -23,10 +23,13 @@ module.exports = function(app, passport) {
     });
   });
 
-  app.post('/signup', function(req, res) {
-    console.log(req.body);
-    //  passport stuff
-  });
+  // process the signup form
+  app.post('/signup', passport.authenticate('local-signup', {
+    successRedirect : '/profile', // redirect to the secure profile section
+    failureRedirect : '/signup', // redirect back to the signup page if there is an error
+    failureFlash : true // allow flash messages
+  }));
+
 
   app.get('/profile', isLoggedIn, function(req, res) {
     res.render('profile.ejs', {
