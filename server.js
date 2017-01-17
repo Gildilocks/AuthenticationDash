cd623034977fbf2994bd96802eb8d87d33277315
Dylan Larrabee <dylan.r.larrabee@gmail.com>
2017-01-16 22:23:14 -0800

    Set up server.js file

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
-oyg
+var app = require('express')();
+var port = process.env.PORT || 8080;
+
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+var dbConfig = require('./config/database.js');
+
+mongoose.connect(dbConfig.url);
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser());
+app.set('view-engine', 'ejs');
+app.use(session({secret: 'shhitsasecret'}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+require('./app/routes.js')(app, passport);
+
+app.listen(port);
+console.log('listening on port:', port);
