e37be0a5cd75ee3ede19f921eb787a0793963118
Dylan Larrabee <dylan.r.larrabee@gmail.com>
2017-01-17 02:23:38 -0800

    Add methods to user schema

--- app/models/user.js
+++ app/models/user.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
 
 var userSchema = mongoose.Schema({
-
   local: {
     email:    String,
     password: String,
@@ -25,5 +24,17 @@ var userSchema = mongoose.Schema({
     email: String,
     name:  String
   }
-
 });
+
+// generating a hash
+userSchema.methods.generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// checking if password is valid
+userSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.local.password);
+};
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('User', userSchema);
