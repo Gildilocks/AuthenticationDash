7b25abc7d80b507c896cd4fcb60a5ad6c18fec61
Dylan Larrabee <dylan.r.larrabee@gmail.com>
2017-01-18 13:45:04 -0800

    Replace local login callback with imported cb

--- config/passport.js
+++ config/passport.js
@@ -31,7 +31,7 @@ module.exports = function(passport) {
     passwordField: 'password',
     passReqToCallback: true
   },
-  LocalCb.signup
+    LocalCb.signup
   ));
 
   //local login ----------------------------------------------------
@@ -41,23 +41,7 @@ module.exports = function(passport) {
     passwordField : 'password',
     passReqToCallback : true // allows us to pass back the entire request to the callback
   },
-  function(req, email, password, done) { // callback with email and password from our form
-    // find a user whose email is the same as the forms email
-    // we are checking to see if the user trying to login already exists
-    User.findOne({'local.email': email}, function(err, user) {
-      if (err) {return done(err);}
-      // if no user is found, return the message
-      if (!user) {
-        return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
-      } else if (!user.validPassword(password)) {
-        return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
-      } else {
-        // all is well, return successful user
-        return done(null, user);
-      }
-    });
-  }
-
+    LocalCb.login
   ));
 
 };
