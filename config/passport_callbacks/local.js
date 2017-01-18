a87d777b0a954a59f5c38775d72ea8102e9ac7f7
Dylan Larrabee <dylan.r.larrabee@gmail.com>
2017-01-18 13:21:17 -0800

    Duplicate local signup callback in separate file

--- config/passport_callbacks/local.js
+++ config/passport_callbacks/local.js
@@ -1,1 +1,41 @@
-local.js
+var User = require('../../app/models/user');
+
+module.exports = {
+  signup: function(req, email, password, done) {
+    // asynchronous
+    // User.findOne wont fire unless data is sent back
+    process.nextTick(function() {
+      // find a user whose email is the same as the forms email
+      // we are checking to see if the user trying to login already exists
+      User.findOne({'local.email': email}, function(err, user) {
+        if (err) {
+          // if there are any errors, return the error
+          return done(err);
+        } 
+        // check to see if theres already a user with that email
+        if (user) {
+          return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+        } else {
+          // if there is no user with that email
+          // create the user
+          var newUser = new User();
+          // set the user's local credentials
+          newUser.local.email = email;
+          newUser.local.password = newUser.generateHash(password);
+          // save the user
+          newUser.save(function() {
+            if (err) {
+              throw err;
+            } else {
+              return done(null, newUser);
+            }
+          }); 
+        }
+
+      });
+    });
+
+  },
+
+  login: null
+}
