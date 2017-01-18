aac8f0bbbe295a61ca75b7a6a8d7fc8d22ae80db
Dylan Larrabee <dylan.r.larrabee@gmail.com>
2017-01-18 13:43:29 -0800

    Replace local signup callback with imported cb

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,10 @@
 // load the strategy
 var LocalStrategy = require('passport-local').Strategy;
+// include local passport callbacks
+var LocalCb = require('./passport_callbacks/local.js');
 // load the user model
 var User = require('../app/models/user');
+
 // expose the function to the app
 module.exports = function(passport) {
   // passport session setup
@@ -28,41 +31,7 @@ module.exports = function(passport) {
     passwordField: 'password',
     passReqToCallback: true
   },
-  function(req, email, password, done) {
-    // asynchronous
-    // User.findOne wont fire unless data is sent back
-    process.nextTick(function() {
-      // find a user whose email is the same as the forms email
-      // we are checking to see if the user trying to login already exists
-      User.findOne({'local.email': email}, function(err, user) {
-        if (err) {
-          // if there are any errors, return the error
-          return done(err);
-        } 
-        // check to see if theres already a user with that email
-        if (user) {
-          return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
-        } else {
-          // if there is no user with that email
-          // create the user
-          var newUser = new User();
-          // set the user's local credentials
-          newUser.local.email = email;
-          newUser.local.password = newUser.generateHash(password);
-          // save the user
-          newUser.save(function() {
-            if (err) {
-              throw err;
-            } else {
-              return done(null, newUser);
-            }
-          }); 
-        }
-
-      });
-    });
-
-  }
+  LocalCb.signup
   ));
 
   //local login ----------------------------------------------------
