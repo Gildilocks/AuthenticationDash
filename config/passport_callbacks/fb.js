3e39b12ac909e99b39015c593f47a71439a90cef
Dylan Larrabee <dylan.r.larrabee@gmail.com>
2017-01-19 16:09:13 -0800

    Extract different variables from profile object

--- config/passport_callbacks/fb.js
+++ config/passport_callbacks/fb.js
@@ -23,12 +23,15 @@ module.exports = {
           // if there is no user found with that facebook id, create them
           var newUser = new User();
 
+          console.log('PROFILE DETAILS:', profile); // I had to print the profile so I could see what information I could grab from it
+          // apperaerntly, even though I specified in the scope that i want 
+
           // set all of the facebook information in our user model
           newUser.facebook.id    = profile.id; // set the users facebook id                   
           newUser.facebook.token = token; // we will save the token that facebook provides to the user                    
-          newUser.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName; // look at the passport user profile to see how names are returned
-          // console.log('EMAILS!!!!!', profile.emails);
-          newUser.facebook.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
+          newUser.facebook.name  = profile.displayName;// + ' ' + profile.name.familyName; // look at the passport user profile to see how names are returned
+          // console.log('EMAILS!!!!!', profile.emails);  //There is no email information in the profile!!!!!????
+          // newUser.facebook.email = profile.emails;//[0].value; // facebook can return multiple emails so we'll take the first
 
           // save our user to the database
           newUser.save(function(err) {
