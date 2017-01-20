e77abf3e11bb90d8c27df5b38e5fc51bee360d0d
Dylan Larrabee <dylan.r.larrabee@gmail.com>
2017-01-19 17:53:10 -0800

    Update scope from 'email' to 'emails'

--- app/routes.js
+++ app/routes.js
@@ -34,8 +34,8 @@ module.exports = function(app, passport) {
     failureFlash: true // allow flash messages
   }));
 
-  // route to process facebook authentication and login
-  app.get('/auth/facebook', passport.authenticate('facebook', {scope: 'email'}));
+  // route to process facebook authentication and login       // try adding different things to the scope!!!! and add them to the database, and to the profile view if successful
+  app.get('/auth/facebook', passport.authenticate('facebook', {scope: ['emails']}));
 
   //
   app.get('/auth/facebook/callback', passport.authenticate('facebook', {
