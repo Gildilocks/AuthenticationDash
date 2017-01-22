39bed9904ca1f142b22e37ed456d15020b653712
Dylan Larrabee <dylan.r.larrabee@gmail.com>
2017-01-21 16:31:24 -0800

    Add google CB that logs profile, token details

--- config/passport_callbacks/google.js
+++ config/passport_callbacks/google.js
@@ -1,5 +1,43 @@
 var User = require('../../app/models/user');
 
 module.exports = {
-  login: null
+  login: function(token, refreshToken, profile, done) {
+    // make the code asynchronous
+    // User.findOne won't fire until we have all our data back from Twitter
+    process.nextTick(function() {
+
+      console.log('GOOGLE PROFILE DETAILS:', profile);
+      console.log('GOOGLE TOKEN DETAILS:', token);
+
+      User.findOne({'google.id': profile.id}, function(err, user) {
+        // if there is an error, stop everything and return that
+        // ie an error connecting to the database
+        if (err) {
+          return done(err);
+        }
+
+        // if the user is found then log them in
+        if (user) {
+          return done(null, user); // user found, return that user
+        } else {
+          // if there is no user, create them
+          // var newUser = new User();
+
+          // set all of the user data that we need
+          // newUser.google.id    = profile.id;
+          // newUser.google.token = token;
+          // newUser.google.name  = profile.displayName;
+          // newUser.google.email = profile.emails[0].value; // pull the first email
+
+          // save our user into the database
+          // newUser.save(function(err) {
+          //   if (err) {
+          //     throw err;
+          //   }
+          //   return done(null, newUser);
+          // });
+        }
+      });
+    });
+  }
 };
