33ad5e65552c776cf53ba172a5074d2df2328c0e
Dylan Larrabee <dylan.r.larrabee@gmail.com>
2017-01-23 14:20:05 -0800

    Complete passport github callback

--- config/passport_callbacks/github.js
+++ config/passport_callbacks/github.js
@@ -1,5 +1,32 @@
 var User = require('../../app/models/user');
 
 module.exports = {
-  login: null
-}
+  login: function(accessToken, refreshToken, profile, done) {
+    console.log('GITHUB PROFILE INFO:', profile);
+
+    User.findOne({'google.id': profile.id}, function (err, user) {  
+      if (err) { // if theres an error call done with the error
+        return done(err);
+      }
+
+      if (user) { // if a user is found (update token if needed) and call done on the user
+
+        return done(null, user);
+
+      } else { // make a user, save it, and pass it to done
+        var newUser = new User();
+
+        newUser.github.id = profile.id;
+
+        newUser.save(function(err) {
+          if (err) {
+            throw err;
+          }
+          return done(null, newUser);
+        });
+      }
+
+
+    });
+  }
+};
