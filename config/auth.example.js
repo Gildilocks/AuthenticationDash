7956e3bf10b3827450495472885950711f69fe4b
Dylan Larrabee <dylan.r.larrabee@gmail.com>
2017-01-23 13:52:41 -0800

    Update auth example

--- config/auth.example.js
+++ config/auth.example.js
@@ -16,6 +16,12 @@ module.exports = {
     'clientID'    : 'your-secret-clientID-here',
     'clientSecret': 'your-client-secret-here',
     'callbackURL' : 'http://localhost:8080/auth/google/callback'
+  }, 
+
+  'githubAuth' : {
+    'clientID': 'GITHUB_CLIENT_ID',
+    'clientSecret': 'GITHUB_CLIENT_SECRET',
+    'callbackURL': "http://localhost:8080/auth/github/callback"
   }
 
 };
