2332a657b8abb1f539aa357b000ebaffe1483d2f
Dylan Larrabee <dylan.r.larrabee@gmail.com>
2017-01-23 15:45:17 -0800

    Overwrite github accessToken at each login

--- config/passport_callbacks/github.js
+++ config/passport_callbacks/github.js
@@ -3,6 +3,7 @@ var User = require('../../app/models/user');
 module.exports = {
   login: function(accessToken, refreshToken, profile, done) {
     console.log('GITHUB PROFILE INFO:', profile);
+    console.log('GITHUB ACCESS TOKEN:', accessToken);
 
     User.findOne({'github.id': profile.id}, function (err, user) {  
       if (err) { // if theres an error call done with the error
@@ -11,7 +12,14 @@ module.exports = {
 
       if (user) { // if a user is found (update token if needed) and call done on the user
 
-        return done(null, user);
+        user.github.token = accessToken;
+
+        user.save(function(err) {
+          if (err) {
+            throw err;
+          }
+          return done(null, user);
+        });
 
       } else { // make a user, save it, and pass it to done
         var newUser = new User();
@@ -19,7 +27,7 @@ module.exports = {
         newUser.github.id = profile.id;
         newUser.github.userName = profile.userName;
         newUser.github.profileUrl = profile.profileUrl;
-        newUser.github.token = token;
+        newUser.github.token = accessToken;
 
         newUser.save(function(err) {
           if (err) {
