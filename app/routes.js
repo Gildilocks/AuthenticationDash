4395d3c8e67d8a19be75e5c178bd5dfb15620517
Dylan Larrabee <dylan.r.larrabee@gmail.com>
2017-01-30 12:57:01 -0800

    Add Authorization routes for loginservices

--- app/routes.js
+++ app/routes.js
@@ -13,6 +13,29 @@ module.exports = function(app, passport) {
     });
   });
 
+ // signup page
+  app.get('/signup', function(req, res) {
+    res.render('signup.ejs', {
+      message: req.flash('signupMessage') // render the page with passed in flash data
+    });
+  });
+
+  // profile page
+  app.get('/profile', isLoggedIn, function(req, res) {
+    res.render('profile.ejs', {
+      user: req.user // get user data out of the request
+    });
+  });
+
+  // logout route
+  app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+  });
+
+
+// local route -------------------------------------------------------
+
   // process the login form
   app.post('/login', passport.authenticate('local-login', {
     successRedirect: '/profile', // redirect to the secure profile section
@@ -20,17 +43,19 @@ module.exports = function(app, passport) {
     failureFlash: true // allow flash messages
   }));
 
-  // signup page
-  app.get('/signup', function(req, res) {
-    res.render('signup.ejs', {
-      message: req.flash('signupMessage') // render the page with passed in flash data
-    });
-  });
-
   // process the signup form
   app.post('/signup', passport.authenticate('local-signup', {
     successRedirect: '/profile', // redirect to the secure profile section
     failureRedirect: '/signup', // redirect back to the signup page if there is an error
+    failureFlash: true // allow flash messages
+  }));
+  //    ####### CONNECT ROUTE #######
+  app.get('/connect/local', function(req, res) {
+    res.render('connect-local.ejs', {message: req.flash('loginMessage')});
+  });
+  app.post('/connect/local', passport.authenticate('local-signup', {
+    successRedirect: '/profile', // redirect to the secure profile section
+    failureRedirect: '/connect/local', // redirect back to the signup page if there is an error
     failureFlash: true // allow flash messages
   }));
 
@@ -42,6 +67,15 @@ module.exports = function(app, passport) {
     successRedirect: '/profile',
     failureRedirect: '/'
   }));
+  //    ####### CONNECT ROUTE #######
+  // send to facebook to do the authentication
+  app.get('/connect/facebook', passport.authorize('facebook', {scope: 'email'}));
+  // handle the callback after facebook has authorized the user
+  app.get('/connect/facebook/callback',
+    passport.authorize('facebook', {
+      successRedirect: '/profile',
+      failureRedirect: '/'
+    }));
 
   // twitter route -------------------------------------------------------
   app.get('/auth/twitter', passport.authenticate('twitter'));
@@ -50,6 +84,16 @@ module.exports = function(app, passport) {
     successRedirect: '/profile',
     failureRedirect: '/'
   }));
+  //    ####### CONNECT ROUTE #######
+  // send to twitter to do the authentication
+  app.get('/connect/twitter', passport.authorize('twitter', {scope: 'email'}));
+  // handle the callback after twitter has authorized the user
+  app.get('/connect/twitter/callback',
+    passport.authorize('twitter', {
+      successRedirect: '/profile',
+      failureRedirect: '/'
+    }));
+
 
   // google route --------------------------------------------------------
   app.get('/auth/google', passport.authenticate('google', {
@@ -61,32 +105,42 @@ module.exports = function(app, passport) {
 
   app.get('/auth/google/callback',
     passport.authenticate('google', {
-      successRedirect : '/profile',
-      failureRedirect : '/'
+      successRedirect: '/profile',
+      failureRedirect: '/'
     })
   );
+  //    ####### CONNECT ROUTE #######
+  // send to google to do the authentication
+  app.get('/connect/google', passport.authorize('google', {
+    accessType: 'offline',
+    prompt: 'consent',
+    scope: ['profile', 'email', 'https://www.googleapis.com/auth/calendar.readonly']
+  }));
+  // the callback after google has authorized the user
+  app.get('/connect/google/callback',
+    passport.authorize('google', {
+      successRedirect: '/profile',
+      failureRedirect: '/'
+    }));
   
-  // google route --------------------------------------------------------
+  // github route --------------------------------------------------------
   app.get('/auth/github', passport.authenticate('github', {scope: ['user:email']}));
 
   app.get('/auth/github/callback',
     passport.authenticate('github', {
-      successRedirect : '/profile',
-      failureRedirect : '/'
+      successRedirect: '/profile',
+      failureRedirect: '/'
     })
   );
-
-
-  app.get('/profile', isLoggedIn, function(req, res) {
-    res.render('profile.ejs', {
-      user: req.user // get user data out of the request
-    });
-  });
-
-  app.get('/logout', function(req, res) {
-    req.logout();
-    res.redirect('/');
-  });
+  //    ####### CONNECT ROUTE #######
+  // send to github to do the authentication
+  app.get('/connect/github', passport.authorize('github', {scope: ['user:email']}));
+  // the callback after github has authorized the user
+  app.get('/connect/github/callback', 
+    passport.authorize('twitter', {
+      successRedirect:'/profile',
+      failureRedirect: '/'
+    }));
 
 };
 
